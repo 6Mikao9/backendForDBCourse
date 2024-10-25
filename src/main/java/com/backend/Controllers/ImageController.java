1904e755760a523893e43d4c0411eb4e0e6f85b5
刘云韬 <22371253@buaa.edu.cn>
2024-10-25 19:13:19 +0800

    fix : 跨域bug

--- src/main/java/com/backend/Controllers/ImageController.java
+++ src/main/java/com/backend/Controllers/ImageController.java
@@ -1,20 +1,23 @@
 package com.backend.Controllers;
 
+import lombok.extern.slf4j.*;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-@Controller
+@RestController
+@Slf4j
+@RequestMapping("/api1")
+
 public class ImageController {
 
     @GetMapping("/images/{imageName:.+}")
---
+++ src/main/java/com/backend/config/MyCorsConfig.java
@@ -0,0 +1,23 @@
+package com.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class MyCorsConfig {
+    @Bean
+    public CorsFilter corsFilter(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://10.192.63.51:8081/");
+        config.addAllowedOrigin("http://localhost:8081/");
+        config.setAllowCredentials(true);
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
