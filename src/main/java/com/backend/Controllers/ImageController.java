5a634f5686c321eedb617f3ed3c3843aaa49dddb
刘云韬 <22371253@buaa.edu.cn>
2024-10-25 14:05:13 +0800

    add : 增加了向前端发送图片的功能

---
+++ src/main/java/com/backend/Controllers/ImageController.java
@@ -0,0 +1,38 @@
+package com.backend.Controllers;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Controller
+public class ImageController {
+
+    @GetMapping("/images/{imageName:.+}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String imageName) {
+        System.out.println("imageName: " + imageName);
+        try {
+            // 从资源目录中加载图片
+            Resource resource = new ClassPathResource("static/images/" + imageName);
+            Path path = Paths.get(resource.getURI());
+            byte[] imageData = Files.readAllBytes(path);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_TYPE, "image/png"); // 根据图片类型修改，例如 image/jpeg
+
+            return new ResponseEntity<>(imageData, headers, HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
